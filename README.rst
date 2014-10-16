b14b4cfb888a72e3ab921d55a35a5ad2be7da256
amitt001 <mail2amit19@gmail.com>
2014-10-16 23:54:28 +0530

    Directory cleaner module. Clean log directory at one.

--- README.rst
+++ README.rst
@@ -1,9 +1,11 @@
 logparlour
 ==========
 
-IRC Log Cleaner. Make your IRC logs beautiful. 
+IRC Log Cleaner. Beautiful IRC logs.
 
-Removes all the unnecessary server messages like others joining/leaving, changing nicks etc. Your local messages from server like you join the channel or changing the password etc are not removed.
+Removes all the unnecessary server messages like others joining/leaving, changing nicks etc. Focus on you important messages. Your local messages from server like you join the channel or changing the password etc are not removed.
+
+**Reduced the size of 30.3 MB of logs to 23.4 MB:** 23% disk usage reduction.
 
 **How to use?**
 
@@ -11,12 +13,18 @@ Run it like this::
 
     python3 logparlour.py -l /destination/of/your/log_file
 
+    python3 logparlour.py -d /destination/of/your/log_directory
+
 or::
 
     ./logparlour.py -l /destination/of/your/log_file
 
-Instead of -l use -h for help.
+    ./logparlour.py -l /destination/of/your/log_directory
 
-INFO:
 
-ONLY TESTED WITH XCHAT. But your logs will never be modified. It creates a new file with name "HotnameOfYourLogFile" and your original logs remain untouched.
+Instead of -l,-d use -h for help.
+
+**INFO:**
+
+ONLY TESTED WITH XCHAT(should work with hexchat also). 
+Can be tried with others also. *Your original logs are never modified. It creates a new file/directory with name "HotnameOfYourLogFile" or "HownameOfYourDirectory" and your original logs remain untouched*.
--- logparlour.py
+++ logparlour.py
@@ -3,7 +3,7 @@
 import os
 from argparse import ArgumentParser
 
-
+###File with same name already exists?
 def existanceCheck(path, filename):
     name = str(path.split("/")[-1])
     path = path.rstrip(name)
@@ -18,12 +18,12 @@ def existanceCheck(path, filename):
             else:
                 return 0
 
-
+###cleaner
 def parlour(path):
-
+    print(path)
     with open(path, "r") as fobj:
 
-        filename = "Hot" + args.FileLocation.split("/")[-1]
+        filename = "Hot" + path.split("/")[-1]
         flag = existanceCheck(path, filename)
         
         if (flag == 0):
@@ -35,16 +35,32 @@ def parlour(path):
                     beauty.write(lines)
     return
 
+# Clening a directory containing log files.
+def logDirectory(path):
+    newpath = path.rstrip(path.split('/')[-1]) + 'Hot' + path.split('/')[-1]
+    os.mkdir(newpath)
+    os.chdir(newpath)
+    for i in os.listdir(path):
+        filepath = path + '/' + i
+        parlour(filepath)
+#        os.remove(filepath)
 
 
 if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument("-l", dest="FileLocation", help="Location of IRC log file requiring  beauty treatment")
+    parser.add_argument("-d", dest="Directory", nargs='?', help="Location directory of IRC log file requiring  beauty treatment")
     args = parser.parse_args()
 
     if (args.FileLocation):
         parlour(args.FileLocation)
 
+    elif(args.Directory):
+        direcpath = args.Directory
+        if args.Directory[-1] == '/':
+            direcpath = args.Directory.rstrip('/')
+        logDirectory(direcpath)
+
     else:
-        print("Complete address of the lady please...(try -h option for help)")
+        print("Hey, Address of file/directory please...(try -h option for help)")
